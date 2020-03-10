ad6cd23ce6cc2e4f7376d835a24db021989567a6
JesseSmart <jesse.oscar.smart@gmail.com>
2020-03-10 11:47:12 +1100

    Worked on menu and scene changing

---
+++ Assets/My Scripts/Jesses Brand/MenuManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public GameObject pnlMain;
+	public GameObject pnlLevelSel;
+	public GameObject pnlCharSel;
+	public GameObject pnlSettings;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void ToLevelSelClicked()
+	{
+		PanelManager(false, true, false, false);
+	}
+
+	public void ToCharSelClicked()
+	{
+		PanelManager(false, false, true, false);
+	}
+
+	public void ToSettingsClicked()
+	{
+		PanelManager(false, false, false, true);
+	}
+
+	public void BackToMenuClicked()
+	{
+		PanelManager(true, false, false, false);
+	}
+
+	public void PanelManager(bool mainB, bool levelB, bool charB, bool settingB)
+	{
+		pnlMain.SetActive(mainB);
+		pnlLevelSel.SetActive(levelB);
+		pnlCharSel.SetActive(charB);
+		pnlSettings.SetActive(settingB);
+	}
+
+	public void SelectLevel(int levelNum)
+	{
+		SceneManager.LoadScene("JesseLevel" + levelNum);
+	}
+
+}
