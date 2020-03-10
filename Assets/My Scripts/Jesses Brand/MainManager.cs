575e558b4b6d55bdfc8950c414ed72000d9dc8d5
JesseSmart <jesse.oscar.smart@gmail.com>
2020-03-10 14:34:26 +1100

    Worked on pause menu

--- Assets/My Scripts/Jesses Brand/MainManager.cs
+++ Assets/My Scripts/Jesses Brand/MainManager.cs
@@ -10,20 +10,25 @@ public class MainManager : MonoBehaviour
 	public GameObject playerSpawnedObj;
 	private GameObject spawnPoint;
 	private CameraController cameraCon;
+
+	public GameObject pnlPause;
     // Start is called before the first frame update
     void Start()
     {
 		playerObj = FindObjectOfType<PlayerMaster>().gameObject;
 		cameraCon = FindObjectOfType<CameraController>();
 		spawnPoint = GameObject.Find("EGO SpawnPoint");
-
+		//pnlPause = GameObject.Find("PausePanel");
 	}
 
 	// Update is called once per frame
 	void Update()
     {
-        
-    }
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			PauseBehaviour();
+		}
+	}
 
 	public void PlayerDeath()
 	{
@@ -39,5 +44,17 @@ public class MainManager : MonoBehaviour
 
 	}
 
-
+	void PauseBehaviour()
+	{
+		if (Time.timeScale > 0)
+		{
+			Time.timeScale = 0f;
+			pnlPause.SetActive(true);
+		}
+		else
+		{
+			Time.timeScale = 1f;
+			pnlPause.SetActive(false);
+		}
+	}
 }
---
+++ Assets/My Scripts/Jesses Brand/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+
+	public GameObject pnlPause;
+
+	private bool audioOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+	public void Resume()
+	{
+		Time.timeScale = 1f;
+		pnlPause.SetActive(false);
+	}
+
+	public void BackToMenu()
+	{
+		Time.timeScale = 1f;
+		pnlPause.SetActive(false);
+		SceneManager.LoadScene("JesseMainMenu");
+	}
+
+	public void AudioPressed()
+	{
+		print("audio");
+		audioOn = !audioOn;
+	}
+}
